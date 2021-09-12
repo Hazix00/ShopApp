1c6812b541a1e3754302010a4b0f337a0a00df01
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-09-12 13:32:42 +0100

    added product listing

--- ShopApp.WebUI/Controllers/HomeController.cs
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShopApp.Core.Contracts;
+using ShopApp.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,27 @@ namespace ShopApp.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productContext;
+        IRepository<ProductCategory> productCategoryContext;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            this.productContext = productContext;
+            this.productCategoryContext = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productContext.Collection().ToList();
+            return View(products);
         }
-
+        public ActionResult Details(string Id)
+        {
+            Product product = productContext.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
