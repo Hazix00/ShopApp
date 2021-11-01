06d391d76eb48efc7eb709c0e63c9429c0556516
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-11-01 17:50:00 +0100

    fixed image height and authentication exeptions

--- ShopApp.WebUI/App_Start/UnityConfig.cs
+++ ShopApp.WebUI/App_Start/UnityConfig.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using ShopApp.Core.Contracts;
 using ShopApp.Core.Models;
 using ShopApp.DataAccess.SQL;
+using ShopApp.WebUI.Controllers;
+using ShopApp.WebUI.Models;
 using System;
-
+using System.Data.Entity;
+using System.Web;
 using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
 
 namespace ShopApp.WebUI
 {
@@ -45,6 +53,34 @@ namespace ShopApp.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
+            //12-1-16 Need this for Identity
+            container.RegisterType<ApplicationDbContext>();
+            container.RegisterType<ApplicationSignInManager>();
+            container.RegisterType<ApplicationUserManager>();
+            container.RegisterType<EmailService>();
+
+            container.RegisterFactory<IAuthenticationManager>(
+                c => HttpContext.Current.GetOwinContext().Authentication);
+
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(
+                new InjectionConstructor(typeof(ApplicationDbContext)));
+
+
+            container.RegisterType<AccountController>(
+                new InjectionConstructor(typeof(ApplicationUserManager), typeof(ApplicationSignInManager)));
+
+            container.RegisterType<AccountController>(
+                    new InjectionConstructor());
+
+            //Identity / Unity stuff below to fix No IUserToken Issue  - http://stackoverflow.com/questions/24731426/register-iauthenticationmanager-with-unity
+            container.RegisterType<DbContext, ApplicationDbContext>(
+                new HierarchicalLifetimeManager());
+            container.RegisterType<UserManager<ApplicationUser>>(
+                new HierarchicalLifetimeManager());
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(
+                new HierarchicalLifetimeManager());
+
+
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
